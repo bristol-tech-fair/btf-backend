d496963da94f3c2f59d82466b7daf50b8aba4c9c
Shane Mitchell <shanesc@gmail.com>
2021-03-24 12:53:40 +0000

    feat: finish crudControllers methods

--- utils/crud.js
+++ utils/crud.js
@@ -1,34 +1,57 @@
-// GET MANY
+// Get Many
 const getMany = model => async (req, res) => {
   try {
-  } catch (err) {}
+    const docs = await model.find();
+    res.json({ data: docs });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// GET ONE
+// Get One
 const getOne = model => async (req, res) => {
   try {
-  } catch (err) {}
+    const doc = await model.findById(req.params.id);
+    if (!doc) {
+      return res.status(404).send('Resource not found');
+    } else {
+      res.json({ data: doc });
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// CREATE ONE
+// Create One
 const createOne = model => async (req, res) => {
   try {
-  } catch (err) {}
+    const doc = await model.create(req.body);
+    res.status(201).json({ data: doc });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
 // UPDATE ONE
 const updateOne = model => async (req, res) => {
   try {
-  } catch (err) {}
+    const doc = await model.updateOne({ _id: req.params.id }, req.body);
+    res.json({ msg: `Resource ID: ${req.params.id} updated`, data: doc });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// DELETE ONE
+// Delete One
 const deleteOne = model => async (req, res) => {
   try {
-  } catch (err) {}
+    const doc = await model.deleteOne({ _id: req.params.id });
+    res.json({ msg: `Resource ID: ${req.params.id} deleted`, data: doc });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// export object
 export default crudControllers = model => {
   return {
     getMany: getMany(model),
