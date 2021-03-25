3054a137d96ed6373bc32752a7f1838e87f3110a
Shane Mitchell <shanesc@gmail.com>
2021-03-25 16:27:38 +0000

    chore: move to src folder, fix issue with default export

---
+++ src/utils/crud.js
@@ -0,0 +1,65 @@
+// Get Many
+const getMany = model => async (req, res) => {
+  try {
+    const docs = await model.find();
+    res.json({ data: docs });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// Get One
+const getOne = model => async (req, res) => {
+  try {
+    const doc = await model.findById(req.params.id);
+    if (!doc) {
+      return res.status(404).send('Resource not found');
+    } else {
+      res.json({ data: doc });
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// Create One
+const createOne = model => async (req, res) => {
+  try {
+    const doc = await model.create(req.body);
+    res.status(201).json({ data: doc });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// UPDATE ONE
+const updateOne = model => async (req, res) => {
+  try {
+    const doc = await model.updateOne({ _id: req.params.id }, req.body);
+    res.json({ msg: `Resource ID: ${req.params.id} updated`, data: doc });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// Delete One
+const deleteOne = model => async (req, res) => {
+  try {
+    const doc = await model.deleteOne({ _id: req.params.id });
+    res.json({ msg: `Resource ID: ${req.params.id} deleted`, data: doc });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const crudControllers = model => {
+  return {
+    getMany: getMany(model),
+    getOne: getOne(model),
+    createOne: createOne(model),
+    updateOne: updateOne(model),
+    deleteOne: deleteOne(model)
+  };
+};
+
+export default crudControllers;
