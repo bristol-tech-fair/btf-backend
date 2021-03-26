4071565a90560197dc3646c212c971e6260ac7fc
Shane Mitchell <47223963+shanesc@users.noreply.github.com>
2021-03-26 17:00:14 +0000

    Feat/add server file (#13)
    * feat: create barebones server.js file
    
    * chore: move server.js to src folder, remove redundant import of dotenv
    
    * feat: add json body parsing middleware

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import postRouter from './resources/post/post.router';
+
+const app = express();
+
+// set PORT value from environment variables or default 3000
+const PORT = process.env.PORT || 3000;
+
+// setup the database
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', err => console.log(err));
+db.once('open', () => console.log(`Connected to database`));
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use('/api/post', postRouter);
+
+// start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
