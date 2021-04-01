67ab71adaf531458f3423ed5ef4c9fc48d97f8a7
Shane Mitchell <shanesc@gmail.com>
2021-04-01 17:38:56 +0100

    feat: add Competition model data types

--- src/resources/competition/competition.model.js
+++ src/resources/competition/competition.model.js
@@ -8,7 +8,9 @@ const competitionSchema = new mongoose.Schema({
   body: {
     type: String,
     required: true
-  }
+  },
+  deadline: Date,
+  image_url: String
 });
 
 export default mongoose.model('Competition', competitionSchema);
