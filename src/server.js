0c80bc9c2977343660e5d77c4e43bd779fba9dd3
Shane Mitchell <shanesc@gmail.com>
2021-04-01 17:39:18 +0100

    feat: add /api/competitions route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import postRouter from './resources/post/post.router';
+import competitionRouter from './resources/competition/competition.router';
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/post', postRouter);
+app.use('/api/competitions', competitionRouter);
 
 // start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
