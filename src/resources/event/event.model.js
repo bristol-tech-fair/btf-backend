ac1debb0e99287eeb3f94e2d96da7f9a1d8306a8
r1903 <55586316+r1903@users.noreply.github.com>
2021-04-02 21:58:51 +0100

    feat: defined schema for event resource

--- src/resources/event/event.model.js
+++ src/resources/event/event.model.js
@@ -3,12 +3,27 @@ import mongoose from 'mongoose';
 const eventSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
-  body: {
+  image: {
     type: String,
     required: true
-  }
+  },
+  date: { 
+    type: Date,
+    required : true,
+    validate: {
+      validator: function(input) {
+        return new Date(input) >= new Date()? true:false;
+      },
+      message: input => `${input} must be greater than or equal to the current date!`
+    }
+  },
+  body:String,
+  facebook: String,
+  linkdin: String,
+  twitter: String,
+  bookmark : String
 });
 
 export default mongoose.model('event', eventSchema);
