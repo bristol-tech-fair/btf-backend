990e35b9fb5dd13a0f38d10a8f89f8f284a8163e
Dave <le_dangereux@hotmail.co.uk>
2021-04-03 07:33:13 +0100

    feat: add LearningResource model, controller and router

---
+++ src/resources/learningResource/learningResource.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const learningResourceSchema = new mongoose.Schema({
+  category: {
+    type: String,
+    enum: ['Coding', 'Maths', 'Electronics'],
+    required: true
+  },
+  minAge: {
+    type: Number,
+    required: true
+  },
+  maxAge: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: true
+  },
+  body: String,
+  tags: {
+    type: [String],
+    required: true
+  }
+});
+
+export default mongoose.model('learningResource', learningResourceSchema);
