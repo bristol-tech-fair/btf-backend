dc3e0f5ec04d6f995203f4d42136bc31cf8bff52
Catherine Howard <51328026+catherinehoward@users.noreply.github.com>
2021-04-09 14:07:39 +0100

    feat: create club schema for MongoDB (#24)
    Co-authored-by: Cath Howard <cath.m.howard@gmail.com>

---
+++ src/resources/club/club.controller.js
@@ -0,0 +1,4 @@
+import crudControllers from '../../utils/crud';
+import club from './club.model';
+
+export default crudControllers(club);
---
+++ src/resources/club/club.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const clubSchema = new mongoose.Schema({
+  image: {
+    type: Image,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,  
+  },
+  title: {
+    type: String,
+    required: true
+  },
+});
+
+export default mongoose.model('club', clubSchema);
